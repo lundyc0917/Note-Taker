a96d5f1a4be0062d8c753555f615e1584d15c61c
lundyc0917 <lundyc0917@gmail.com>
2021-01-19 15:48:00 -0500

    server file created

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// create the express server
+const app = express();
+
+// Set inital port to 8080.
+const PORT = process.env.PORT || 3000;
+
+
